fa8051492b854ed7597f10d952b7900d1403012a
Kali89 <matthewsharpe3@gmail.com>
2012-10-21 16:44:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimalAttack::Application.config.secret_token = '0971a186968dcdd2e436cd98c828787c65d0ed03a61210ad979e4d3005a889711813be77351738f2bc26abbe79d010afef04c80aa1aaa743543b81f8662b527c'
