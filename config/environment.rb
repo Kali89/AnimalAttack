11dd042cfff8b16dfe0670224ad22d334b524a6e
Kali89 <matthewsharpe3@gmail.com>
2012-11-04 02:14:25 +0000

    Trying to find the correct AWS S3 Region

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 AnimalAttack::Application.initialize!
 
-AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+AWS::S3::DEFAULT_HOST.replace "s3-eu-west-1.amazonaws.com"
 
--- config/environment.rb~
+++ config/environment.rb~
@@ -4,3 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 AnimalAttack::Application.initialize!
 
+AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+
