b0249b8b19a9cdbe9c7186d86904a39f0c1328c4
Kali89 <matthewsharpe3@gmail.com>
2012-11-08 21:58:57 +0000

    Tried to implement jQuery on help page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 	respond_to :html, :js
-	before_filter :myvars, :before => :index
+	before_filter :myvars, :only => :index
 	
 	def myvars	
 		@allAnimals = Animal.all
