de2a26d3a3402272a111255008facad43adccdd6
Kali89 <matthewsharpe3@gmail.com>
2012-11-12 00:13:49 +0000

    Made the render bit a tad nicer

---
+++ app/assets/javascripts/centre.js
@@ -0,0 +1,8 @@
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).outerHeight() - this.outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).outerWidth() - this.outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
