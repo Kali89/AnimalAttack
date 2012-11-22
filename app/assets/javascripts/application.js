f8aedfba76ba965a486671616c85611fa5f15fc4
Kali89 <matthewsharpe3@gmail.com>
2012-11-22 23:36:47 +0000

    Changed custom javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).outerHeight() - this.outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).outerWidth() - this.outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
 
 
 
